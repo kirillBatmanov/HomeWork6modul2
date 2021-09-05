539de84531dee650c9844bd4c8f50892308b8a7a
Kirill Batmanov <BatmanovKS@icloud.com>
2021-09-05 16:40:40 +0200

    Interface settings and editViewController

--- HomeWork6modul2/ViewController.swift
+++ HomeWork6modul2/ViewController.swift
@@ -9,11 +9,92 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redTF: UITextField!
+    @IBOutlet var greenTF: UITextField!
+    @IBOutlet var blueTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       colorView.layer.cornerRadius = 15
+       setColor()
+       setValue(for: redLabel, greenLabel, blueLabel)
+       setValue(for: redTF, greenTF, blueTF)
+        
+        
     }
 
-
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenLabel.text = string(from: greenSlider)
+        default:
+            blueLabel.text = string(from: blueSlider)
+        }
+        
+        switch sender {
+        case redSlider:
+            redTF.text = string(from: redSlider)
+        case greenSlider:
+            greenTF.text = string(from: greenSlider)
+        default:
+            blueTF.text = string(from: blueSlider)
+        }
+    }
+    @IBAction func doneButtonPressed(_ sender: UIButton) {
+    }
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redLabel:
+                redLabel.text = string(from: redSlider)
+            case greenLabel:
+                greenLabel.text = string(from: greenSlider)
+            default:
+                blueLabel.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func setValue(for textFields: UITextField...) {
+        textFields.forEach { textField in
+            switch textField {
+            case redTF:
+                redTF.text = string(from: redSlider)
+            case greenTF:
+                greenTF.text = string(from: greenSlider)
+            default:
+                blueTF.text = string(from: blueSlider)
+            }
+        }
+    }
 }
 
---
+++ HomeWork6modul2/mainColorizedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainColorizedViewController.swift
+//  HomeWork6modul2
+//
+//  Created by Кирилл Батманов on 05.09.2021.
+//
+
+import UIKit
+
+class MainColorizedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
